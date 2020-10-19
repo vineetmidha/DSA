90570e0d2d4f16cfa473214ae5976bd1976613be
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-10-19 23:15:56 +0530

    Create LinkedListDeletionO1.cpp

---
+++ LinkedListDeletionO1.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+/*
+Write a function to delete a node in a singly-linked list. 
+You will not be given access to the head of the list. 
+Instead, you will be given access to the node to be deleted directly.
+It is guaranteed that the node to be deleted is not a tail node in the list.
+*/
+
+// 
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;        
+    }
+};
