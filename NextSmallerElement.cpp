23d6c16daef45a5a8f070ee62a5e95e239fae3f2
Vineet Midha <vineet.midha@yahoo.com>
2020-11-14 02:50:09 +0530

    Update NextSmallerElement.cpp

--- NextSmallerElement.cpp
+++ NextSmallerElement.cpp
@@ -48,6 +48,32 @@ int main()
     nextSmallerElement(v);
 }
 
+// Solution by Hacker Earth
+
+    vector<int> prevSmaller(vector<int> &A) {
+        stack<int> s;
+        int n = A.size();
+        vector <int> res;
+        for (int i = 0; i < n; i ++) {
+            if (s.empty()) {
+                res.push_back(-1);
+                s.push(A[i]);
+                continue;
+            }
+            while (!s.empty() && s.top() >= A[i]) {
+                s.pop();
+            }
+            if (s.empty()) {
+                res.push_back(-1);
+            } else {
+                res.push_back(s.top());
+            }
+            s.push(A[i]);
+        }
+        return res;
+    }
+
+
 /*
 Input:
 8
